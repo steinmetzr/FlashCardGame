f100e36fa416098d0db871c5f6556fb3dec1f8fb
steinmetzr <steinmetzr@wit.edu>
2015-03-31 00:02:53 -0400

    work in progress

--- flashcard/src/com/example/flashcard/MainActivity.java
+++ flashcard/src/com/example/flashcard/MainActivity.java
@@ -4,31 +4,149 @@ import java.util.ArrayList;
 import java.util.List;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.TextView;
 
 public class MainActivity extends Activity {
+	Button addFile, removeFile, option;
+	ListCardAdapter adapter;
+	ListView listView;
+	List<ListCard> list;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		
-		Button addFile = (Button) findViewById(R.id.addButton);
-		Button removeFile = (Button) findViewById(R.id.removeButton);
-		Button option = (Button) findViewById(R.id.optionButton);
-		List<ListCard> list = new ArrayList<ListCard>();
+		addFile = (Button) findViewById(R.id.addButton);
+		removeFile = (Button) findViewById(R.id.removeButton);
+		option = (Button) findViewById(R.id.optionButton);
+		list = new ArrayList<ListCard>();
+		adapter = new ListCardAdapter(this, 0, list);
+		listView = (ListView) findViewById(R.id.listView1);
+		listView.setAdapter(adapter);
 		
 		TextView title = (TextView) findViewById(R.id.files);
 		title.setText(Tools.underLine(title.getText().toString()));
-		
+
 		addFile.setOnClickListener(new OnClickListener(){
+			@Override
+			public void onClick(View v){
+				//TODO
+
+			}
+		});
+
+		removeFile.setOnClickListener(new OnClickListener(){
+			@Override
+			public void onClick(View v){
+				Log.v("click", "remove button is clicked");
+				//TODO
+			}
+		});
+
+		option.setOnClickListener(new OnClickListener(){
+			@Override
+			public void onClick(View v){
+				Log.v("click", "option button is clicked");
+				Tools.startIntent(MainActivity.this, Options.class);
+			}
+		});
+			
+		listView.setOnItemClickListener(new OnItemClickListener(){
+			@Override
+			public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+				ListCard temp = (ListCard)parent.getItemAtPosition(position);
+				Log.v("click", "add button is clicked");
+				Bundle bundle = new Bundle();
+				bundle.putString("filename", temp.front);
+				Tools.startIntent(MainActivity.this, FlashCardActivity.class, bundle);
+			}
+
+		});
+
+		addFile.setOnClickListener(new OnClickListener(){
+			@Override
+			public void onClick(View v){
+				Log.v("click", "add button is clicked");
+
+				// get prompts.xml view
+				LayoutInflater layoutInflater = LayoutInflater.from(context);
+				View promptView = layoutInflater.inflate(R.layout.add_file, null);
+				View messageView = layoutInflater.inflate(R.layout.message, null);
+				AlertDialog.Builder addPrompt = new AlertDialog.Builder(context);
+
+				final AlertDialog.Builder message = new AlertDialog.Builder(context);
+				message.setView(messageView);
+				TextView ms = (TextView) messageView.findViewById(R.id.message);
+				ms.setText("File names cannont be empty!");
+				message.setTitle("Warning")
+				.setCancelable(true)
+				.setNeutralButton("OK",
+						new DialogInterface.OnClickListener() {
+					public void onClick(DialogInterface dialog, int id) {
+						dialog.cancel();
+					}
+				});
+
+				// set prompts.xml to be the layout file of the alertdialog builder
+				addPrompt.setView(promptView);
+				addPrompt.setTitle("Add File");
+				final EditText FInput = (EditText) promptView.findViewById(R.id.editFileName);
+
+				// setup a dialog window
+				addPrompt
+				.setCancelable(true)
+				.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+					public void onClick(DialogInterface dialog, int id) {
+						// get user input and set it to result
+
+						if(FInput.getText().toString().trim().length() == 0) {
+							AlertDialog warning = message.create();
+							warning.show();
+						}
+						else {
+							ListFile temp = new ListFile();
+							temp.id = list.size();
+							temp.front = FInput.getText().toString();
+							list.add(temp);
+							adapter.notifyDataSetChanged();
+						}
+
+					}
+				})
+				.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+					public void onClick(DialogInterface dialog,	int id) { 
+						dialog.cancel(); 
+					}
+				});
+
+				// create an alert dialog
+				AlertDialog alert = addPrompt.create();
+				alert.show();
+			}
+		});
+
+		removeFile.setOnClickListener(new OnClickListener(){
+			@Override
+			public void onClick(View v){
+				Log.v("click", "remove button is clicked");
+			}
+		});
+	}
+		/*addFile.setOnClickListener(new OnClickListener(){
 			@Override
 			public void onClick(View v){
 				Log.v("click", "add button is clicked");
@@ -75,7 +193,7 @@ public class MainActivity extends Activity {
 	    ListCardAdapter adapter = new ListCardAdapter(this, 0, list);
 		ListView listView = (ListView) findViewById(R.id.listView1);
 		listView.setAdapter(adapter);
-	}
+	}*/
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
